a04a7b65edbfea8eb20ae1ce72d3be78afb83aa5
Ronny Perez <ronnypdev@gmail.com>
2024-07-22 12:14:17 -0400

    Implemented Nav section

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,49 @@
+import {
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  Services,
+  SpecialOffer,
+  CustomerReviews,
+  Subscribe,
+  Footer
+} from "./sections";
+import Nav from "./components/Nav";
+
 export const App = () => {
   return (
     <main className="relative">
-      Nav
+      <Nav/>
       <section className="xl:padding-1 wide:padding-r padding-b">
-        Hero
+        <Hero />
       </section>
 
       <section className="padding">
-        popular products
+        <PopularProducts />
       </section>
 
       <section className="padding">
-        SuperQuality
+        <SuperQuality />
       </section>
 
       <section className="padding-x py-10">
-        Services
+        <Services />
       </section>
 
       <section className="padding">
-        SpecialOffer
+        <SpecialOffer />
       </section>
 
       <section className="padding bg-pale-blue">
-        CustomerReviews
+        <CustomerReviews />
       </section>
 
       <section className="padding-x sm:py-32 py-16 w-full">
-        Subscribe
+        <Subscribe />
       </section>
 
       <section className="padding-x padding-t pb-8">
-        Footer
+        <Footer />
       </section>
     </main>
   )
